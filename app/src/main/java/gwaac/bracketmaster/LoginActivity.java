10cdf9780442ab94e20ea7469c9711f28324c11f
Charlie Albright <albrightc25@gmail.com>
2015-10-18 23:31:55 -0500

    Initial commit of app redesign.

---
+++ app/src/main/java/gwaac/bracketmaster/LoginActivity.java
@@ -0,0 +1,123 @@
+package gwaac.bracketmaster;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    final static String PREFS_NAME = "BracketMasterPrefs";
+
+    private String mUserEmail;
+    private String mUserPassword;
+
+    private EditText mEmailField;
+    private EditText mPasswordField;
+    private Button mLoginButton;
+    private Button mRegisterButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        checkFirstTimeSetup();
+
+        mEmailField = (EditText)findViewById(R.id.email_field);
+        mPasswordField = (EditText)findViewById(R.id.password_field);
+        mLoginButton = (Button)findViewById(R.id.login);
+        mRegisterButton = (Button)findViewById(R.id.register);
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tryLogin(view);
+            }
+        });
+
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    public void checkFirstTimeSetup() {
+        SharedPreferences sharedPreferences = getSharedPreferences(PREFS_NAME, 0);
+        if (sharedPreferences.getBoolean("first_launch", true)) {
+            // First launch, save some default prefs
+            Log.v("[checkFirstTimeSetup]", "First Launch!");
+            sharedPreferences.edit().putBoolean("first_launch", false).apply();
+            sharedPreferences.edit().putString("user_email", "admin@bracketmaster.com").apply();
+            sharedPreferences.edit().putString("user_password", "fuckdapolice").apply();
+        } else {
+            mUserEmail = sharedPreferences.getString("user_email", "err");
+            mUserPassword = sharedPreferences.getString("user_password", "err");
+        }
+    }
+
+    public void tryLogin(View view) {
+
+        String email = mEmailField.getText().toString();
+        String password = mPasswordField.getText().toString();
+
+        if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            Snackbar.make(view, "Username or Password is empty.", Snackbar.LENGTH_LONG).setAction("OK", new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                }
+            }).setActionTextColor(getResources().getColor(R.color.colorPrimary)).show();
+        } else {
+            if (TextUtils.equals(email, mUserEmail) && TextUtils.equals(password, mUserPassword)) {
+                // Correct Login
+                Snackbar.make(view, "Credentials correct!", Snackbar.LENGTH_LONG).setAction("SWEET!", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                    }
+                }).setActionTextColor(getResources().getColor(R.color.colorPrimary)).show();
+
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+            } else {
+                // Incorrect Login
+                Snackbar.make(view, "Username or Password incorrect.", Snackbar.LENGTH_LONG).setAction("OK", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                    }
+                }).setActionTextColor(getResources().getColor(R.color.colorPrimary)).show();
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        // getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/gwaac/bracketmaster/MainActivity.java
@@ -0,0 +1,35 @@
+package gwaac.bracketmaster;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "This is the main page of the app.", Snackbar.LENGTH_LONG)
+                        .setAction("OK", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                            }
+                        }).setActionTextColor(getResources().getColor(R.color.colorPrimary)).show();
+            }
+        });
+    }
+
+}
