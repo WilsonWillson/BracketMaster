3ef9f2a3c1a054de9f55e14e3bda62046d7acd82
aryamccarthy <arya.mccarthy@gmail.com>
2015-10-26 12:27:15 -0500

    Material color changes

--- app/src/main/java/gwaac/bracketmaster/CreationActivity.java
+++ app/src/main/java/gwaac/bracketmaster/CreationActivity.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.text.ParseException;
+
 public class CreationActivity extends AppCompatActivity {
 
     private EditText mTournamentNameField;
@@ -14,11 +16,15 @@ public class CreationActivity extends AppCompatActivity {
     private EditText mStartTimeField;
     private Button mCreateButton;
 
+    private Notifier notifier;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+
+        notifier = new Notifier(this);
 
         mTournamentNameField = (EditText) findViewById(R.id.tournament_name);
         mGameNameField = (EditText) findViewById(R.id.game_name);
@@ -29,8 +35,24 @@ public class CreationActivity extends AppCompatActivity {
         mCreateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /* Submit that stuff. */
+                notifier.setView(v);
+                TournamentData td = parseTournament();
+                // TODO: submit stuff.
+                finish();
             }
         });
+    }
+
+    public TournamentData parseTournament() {
+        TournamentData td = new TournamentData();
+        td.setTournament(mTournamentNameField.getText().toString());
+        td.setGame(mGameNameField.getText().toString());
+        try {
+            td.setStartTime(mStartTimeField.getText().toString());
+        } catch (ParseException nfe) {
+            notifier.alertWithConfirmation("Inadmissible date format, or date has passed.");
+            return new TournamentData();
+        }
+        return td;
     }
 }
--- app/src/main/java/gwaac/bracketmaster/TournamentData.java
+++ app/src/main/java/gwaac/bracketmaster/TournamentData.java
@@ -1,5 +1,7 @@
 package gwaac.bracketmaster;
 
+import java.text.ParseException;
+
 /**
  * Created by Arya on 10/25/15.
  */
@@ -8,4 +10,32 @@ public class TournamentData {
     private String game;
     private String description;
     private String startTime;
+
+    public TournamentData() {
+        tournament = "";
+        game = "";
+        description = "";
+        startTime = "";
+    }
+
+    public void setTournament(String tournament) {
+        this.tournament = tournament;
+    }
+
+    public void setGame(String game) {
+        this.game = game;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStartTime(String startTime) throws ParseException {
+        verifyDateTimeFormat(startTime);
+        this.startTime = startTime;
+    }
+
+    public void verifyDateTimeFormat(String startTime) throws ParseException {
+        // TODO: needs implementation.
+    }
 }
