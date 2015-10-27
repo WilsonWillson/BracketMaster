38955d4c6d42216504ab56d91ce7b2d538c83763
aryamccarthy <arya.mccarthy@gmail.com>
2015-10-27 17:20:35 -0500

    Set MainActivity as the /main activity/.
    Also used the Default preferences instead of some special file.

--- app/src/main/java/gwaac/bracketmaster/MainActivity.java
+++ app/src/main/java/gwaac/bracketmaster/MainActivity.java
@@ -1,5 +1,6 @@
 package gwaac.bracketmaster;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -11,6 +12,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.firebase.client.Firebase;
+
 public class MainActivity extends AppCompatActivity {
 
     Notifier notifier;
@@ -21,7 +24,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        loginIfNeeded();
 
         notifier = new Notifier(this);
 
@@ -48,16 +52,33 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 notifier.setView(v);
-                notifier.alertWithConfirmation("Needs implementation.");
-                Log.v("[Stuff]", "HELLO!");
                 segueToCreation();
             }
         });
     }
 
-    private void segueToCreation() {
-        Intent intent = new Intent(this, CreationActivity.class);
+    private void loginIfNeeded() {
+        if (!loggedIn()) {
+            segueToLogin();
+        }
+    }
+
+    private boolean loggedIn() {
+        Firebase ref = ((BracketMasterApplication)getApplication()).myFirebaseRef;
+        return ref.getAuth() != null;
+    }
+
+    private void segueToLogin() {
+        segue(LoginActivity.class);
+    }
+
+    private void segue(Class<?> cls) {
+        Intent intent = new Intent(this, cls);
         startActivity(intent);
+    }
+
+    private void segueToCreation() {
+        segue(CreationActivity.class);
     }
 
     @Override
