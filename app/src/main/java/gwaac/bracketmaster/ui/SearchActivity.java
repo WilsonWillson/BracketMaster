0bf7923e0795e6193be4cf90579e730509f0827f
Charlie Albright <albrightc25@gmail.com>
2015-11-18 20:53:52 -0600

    updated SearchActivity for search functionality

--- app/src/main/java/gwaac/bracketmaster/ui/SearchActivity.java
+++ app/src/main/java/gwaac/bracketmaster/ui/SearchActivity.java
@@ -1,21 +1,43 @@
 package gwaac.bracketmaster.ui;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import gwaac.bracketmaster.R;
 
+import butterknife.ButterKnife;
+import butterknife.Bind;
+
 public class SearchActivity extends AppCompatActivity {
+
+    @Bind(R.id.search_type_spinner) Spinner mSearchTypeSpinner;
+    @Bind(R.id.search_text_layout) TextInputLayout mSearchStringTextView;
+    @Bind(R.id.search_button) Button mSearchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
+        ButterKnife.bind(this);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.search_types, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSearchTypeSpinner.setAdapter(adapter);
+
+        mSearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /**
+                 * collect search type and search string and send in intent to SearchResultActivity
+                 */
+            }
+        });
     }
 
 }
