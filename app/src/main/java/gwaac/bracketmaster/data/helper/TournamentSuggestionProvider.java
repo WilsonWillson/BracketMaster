afd9d40a3053fa1928d608941182704d072636dd
aryamccarthy <arya.mccarthy@gmail.com>
2015-11-24 16:42:56 -0600

    Search suggestions are back.
    Also, Filet-O-Fish.

--- app/src/main/java/gwaac/bracketmaster/data/helper/TournamentSuggestionProvider.java
+++ app/src/main/java/gwaac/bracketmaster/data/helper/TournamentSuggestionProvider.java
@@ -6,7 +6,7 @@ import android.content.SearchRecentSuggestionsProvider;
  * Created by Arya on 11/19/15.
  */
 public class TournamentSuggestionProvider extends SearchRecentSuggestionsProvider {
-    public final static String AUTHORITY = "gwaac.bracketmaster.data.helper.MySuggestionProvider";
+    public final static String AUTHORITY = "gwaac.bracketmaster.data.helper.TournamentSuggestionProvider";
     public final static int MODE = DATABASE_MODE_QUERIES;
 
     public TournamentSuggestionProvider() {
