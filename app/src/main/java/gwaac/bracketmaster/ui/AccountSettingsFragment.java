e3be95e2ed724d05eab427ed9895f408a8f424e1
aryamccarthy <arya.mccarthy@gmail.com>
2015-12-02 02:30:17 -0600

    User account page added

--- app/src/main/java/gwaac/bracketmaster/ui/AccountSettingsFragment.java
+++ app/src/main/java/gwaac/bracketmaster/ui/AccountSettingsFragment.java
@@ -1,14 +1,41 @@
 package gwaac.bracketmaster.ui;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.Query;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import gwaac.bracketmaster.BracketMasterApplication;
 import gwaac.bracketmaster.R;
 
 public class AccountSettingsFragment extends android.support.v4.app.Fragment {
+
+    @Bind(R.id.logout_from_acct_page)
+    Button logout_button;
+    @Bind(R.id.user_label)
+    TextView user_label;
+
+    @OnClick(R.id.logout_from_acct_page)
+    public void logout() {
+        Firebase ref = ((BracketMasterApplication) getActivity().getApplication()).myFirebaseRef;
+        ref.unauth();
+        Intent intent = new Intent(this.getActivity(), LoginActivity.class);
+        startActivity(intent);
+        getActivity().finish();
+    }
 
     public AccountSettingsFragment() {
         // Required empty public constructor
@@ -24,7 +51,43 @@ public class AccountSettingsFragment extends android.support.v4.app.Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account_settings, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_account_settings, container, false);
+        ButterKnife.bind(this, view);
+        Firebase firebase = ((BracketMasterApplication)getActivity().getApplication()).myFirebaseRef;
+        String uid = firebase.getAuth().getUid();
+
+        Query query = firebase.child("users").orderByKey().startAt(uid).endAt(uid);
+
+        query.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                user_label.setText(dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+
+        return view;
     }
 
 
--- app/src/main/java/gwaac/bracketmaster/ui/AccountTournamentsFragment.java
+++ app/src/main/java/gwaac/bracketmaster/ui/AccountTournamentsFragment.java
@@ -1,14 +1,37 @@
 package gwaac.bracketmaster.ui;
 
 
+import android.app.DownloadManager;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.Query;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import gwaac.bracketmaster.BracketMasterApplication;
 import gwaac.bracketmaster.R;
+import gwaac.bracketmaster.data.adapter.TournamentAdapter;
+import gwaac.bracketmaster.data.helper.TournamentProperties;
+import gwaac.bracketmaster.data.model.Tournament;
 
 public class AccountTournamentsFragment extends android.support.v4.app.Fragment {
+
+    @Bind(R.id.my_tournaments_recycler_view)
+    RecyclerView mRecyclerView;
+    List<Tournament> tournaments;
+
 
     public AccountTournamentsFragment() {
         // Required empty public constructor
@@ -18,13 +41,60 @@ public class AccountTournamentsFragment extends android.support.v4.app.Fragment 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        initData();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_account_tournaments, container, false);
+        View view = inflater.inflate(R.layout.fragment_account_tournaments, container, false);
+        ButterKnife.bind(this, view);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(layoutManager);
+        TournamentAdapter tournamentAdapter = new TournamentAdapter(this.getActivity(), tournaments);
+        mRecyclerView.setAdapter(tournamentAdapter);
+
+
+        return view;
     }
 
+    private void initData() {
+        if (tournaments == null) {
+            tournaments = new ArrayList<>();
+        }
+        Firebase ref = ((BracketMasterApplication)getActivity().getApplication()).myFirebaseRef;
+        String uid = ref.getAuth().getUid();
+        Query query = ref.child("tournaments").orderByChild("ownerID").equalTo(uid);
+        query.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                TournamentProperties properties = dataSnapshot.getValue(TournamentProperties.class);
+                Tournament tournament = TournamentProperties.toTournament(properties);
 
+                tournaments.add(tournament);
+                mRecyclerView.getAdapter().notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+    }
 }
