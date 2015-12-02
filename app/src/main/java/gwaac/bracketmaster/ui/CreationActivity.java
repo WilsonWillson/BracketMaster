c3c89ed9cc4642c94f32ddde0d6e6b3fc51cae97
aryamccarthy <arya.mccarthy@gmail.com>
2015-12-01 22:08:20 -0600

    Creation validation

--- app/src/main/java/gwaac/bracketmaster/ui/CreationActivity.java
+++ app/src/main/java/gwaac/bracketmaster/ui/CreationActivity.java
@@ -1,5 +1,6 @@
 package gwaac.bracketmaster.ui;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
@@ -29,6 +30,7 @@ import gwaac.bracketmaster.data.helper.CalendarHelper;
 import gwaac.bracketmaster.data.model.Tournament;
 import gwaac.bracketmaster.ui.modal.DatePickerFragment;
 import gwaac.bracketmaster.R;
+import gwaac.bracketmaster.ui.modal.Notifier;
 import gwaac.bracketmaster.ui.modal.TimePickerFragment;
 
 public class CreationActivity extends AppCompatActivity implements DatePickerFragment.OnDateChosenListener, TimePickerFragment.OnTimeChosenListener {
@@ -45,12 +47,14 @@ public class CreationActivity extends AppCompatActivity implements DatePickerFra
     @Bind(R.id.create_tournament) Button mCreateButton;
 
     private GameImageLoader mGameImageLoader;
+    private Notifier mNotifier;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
         ButterKnife.bind(this);
+        mNotifier = new Notifier(this, this.getCurrentFocus());
 
         mGameImageLoader = new GameImageLoader(this);
 
@@ -121,6 +125,9 @@ public class CreationActivity extends AppCompatActivity implements DatePickerFra
                 String description = mDescriptionField.getText().toString();
                 String owner = PreferenceManager.getDefaultSharedPreferences(CreationActivity.this).getString("uid", "null");
                 if (owner.equals("null")) Log.i("[submitting tourney]", "Nobody's logged in.");
+                if (!validateInputs()) {
+                    return;
+                }
                 Tournament tournament = new Tournament()
                         .setOwner(owner)
                         .setDescription(description)
@@ -182,5 +189,40 @@ public class CreationActivity extends AppCompatActivity implements DatePickerFra
             default:
                 Log.d("[onTimeSet]", "This flag has not been handled.");
         }
+    }
+
+    private boolean validateInputs()
+    {
+        String tournamentName = mTournamentNameField.getText().toString();
+        String gameName = mGameSpinnerButton.getEditText().getText().toString();
+        String description = mDescriptionField.getText().toString();
+        if (tournamentName.length() == 0) {
+            mNotifier.alertWithConfirmation("The tournament name is incomplete.");
+            return false;
+        }
+        if (description.length() == 0) {
+            mNotifier.alertWithConfirmation("The description is incomplete.");
+            return false;
+        }
+        if (gameName.length() == 0) {
+            mNotifier.alertWithConfirmation("You must choose a type of game.");
+            return false;
+        }
+        if (mDatePickerStartButton.getEditText().getText().length() == 0 || mTimePickerStartButton.getEditText().getText().length() == 0) {
+            mNotifier.alertWithConfirmation("The start time is not set.");
+            return false;
+        }
+        if (mDatePickerEndButton.getEditText().getText().length() == 0 || mTimePickerEndButton.getEditText().getText().length() == 0) {
+            mNotifier.alertWithConfirmation("The end time is not set.");
+            return false;
+        }
+        if (!end.after(start)) {
+            mNotifier.alertWithConfirmation("The end time must be after the start time.");
+            return false;
+        }
+
+
+        return true;
+
     }
 }
