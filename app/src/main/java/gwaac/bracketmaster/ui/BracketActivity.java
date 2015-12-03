63d48c5d85ed092bc7ce55f54cdca1a8890b3c09
Charlie Albright <albrightc25@gmail.com>
2015-12-03 02:22:45 -0600

    added pagination to bracket view

--- app/src/main/java/gwaac/bracketmaster/ui/BracketActivity.java
+++ app/src/main/java/gwaac/bracketmaster/ui/BracketActivity.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -19,6 +20,8 @@ import com.google.gson.Gson;
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import gwaac.bracketmaster.BracketMasterApplication;
 import gwaac.bracketmaster.R;
 import gwaac.bracketmaster.data.model.Match;
@@ -37,11 +40,20 @@ public class BracketActivity extends AppCompatActivity {
     private Match mMatch;
     private TextView mNoResultsLabel;
     private RecyclerView mRecyclerView;
+    private RecyclerView mRecyclerViewTwo;
+
+    private BracketAdapter mRoundOneAdapter;
+    private BracketAdapter mRoundTwoAdapter;
+
+    @Bind(R.id.fab_next) FloatingActionButton mNextButton;
+    @Bind(R.id.fab_prev) FloatingActionButton mPrevButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bracket);
+
+        ButterKnife.bind(this);
 
         mNoResultsLabel = (TextView)findViewById(R.id.bracket_no_matches_label);
 
@@ -57,10 +69,10 @@ public class BracketActivity extends AppCompatActivity {
             } else {
                 if (mTournament.getMatchList().size() > 0) {
                     mRecyclerView = (RecyclerView) findViewById(R.id.bracket_list);
-                    StaggeredGridLayoutManager mStaggeredLayoutManager = new StaggeredGridLayoutManager(1, LinearLayoutManager.VERTICAL);
+                    StaggeredGridLayoutManager mStaggeredLayoutManager = new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL);
                     mRecyclerView.setLayoutManager(mStaggeredLayoutManager);
-                    BracketAdapter mAdapter = new BracketAdapter(this, mTournament.getMatchList(), TextUtils.equals(owner, uid));
-                    mRecyclerView.setAdapter(mAdapter);
+                    mRoundOneAdapter = new BracketAdapter(this, mTournament.getMatchList(), TextUtils.equals(owner, uid));
+                    mRecyclerView.setAdapter(mRoundOneAdapter);
                 } else {
                     displayNoResults();
                 }
@@ -92,8 +104,27 @@ public class BracketActivity extends AppCompatActivity {
             mMatch.setPlayer2(username);
             List<Match> matches = new ArrayList<>();
             matches.add(mMatch);
-            BracketAdapter adapter = new BracketAdapter(this, matches, true);
-            mRecyclerView.swapAdapter(adapter, false);
+
+            mRecyclerViewTwo = (RecyclerView) findViewById(R.id.bracket_list_two);
+            StaggeredGridLayoutManager mStaggeredLayoutManager = new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL);
+            mRecyclerViewTwo.setLayoutManager(mStaggeredLayoutManager);
+            mRoundOneAdapter = new BracketAdapter(this, matches, true);
+            mRecyclerViewTwo.setAdapter(mRoundOneAdapter);
+
+            mNextButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mRecyclerView.setVisibility(View.GONE);
+                    mRecyclerViewTwo.setVisibility(View.VISIBLE);
+                }
+            });
+            mPrevButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mRecyclerView.setVisibility(View.VISIBLE);
+                    mRecyclerViewTwo.setVisibility(View.GONE);
+                }
+            });
             return;
         }
 
