3a2b84b427bc5a6bd9bcd3e52deacef4756bde85
Charlie Albright <albrightc25@gmail.com>
2015-12-02 21:30:53 -0600

    Added dialog for change email

--- app/src/main/java/gwaac/bracketmaster/ui/AccountSettingsFragment.java
+++ app/src/main/java/gwaac/bracketmaster/ui/AccountSettingsFragment.java
@@ -1,11 +1,16 @@
 package gwaac.bracketmaster.ui;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -24,19 +29,10 @@ import gwaac.bracketmaster.R;
 
 public class AccountSettingsFragment extends android.support.v4.app.Fragment {
 
-    @Bind(R.id.logout_from_acct_page)
-    Button logout_button;
-    @Bind(R.id.user_label)
-    TextView user_label;
+    public static final String TAG = AccountSettingsFragment.class.getSimpleName();
 
-    @OnClick(R.id.logout_from_acct_page)
-    public void logout() {
-        Firebase ref = ((BracketMasterApplication) getActivity().getApplication()).myFirebaseRef;
-        ref.unauth();
-        Intent intent = new Intent(this.getActivity(), LoginActivity.class);
-        startActivity(intent);
-        getActivity().finish();
-    }
+    @Bind(R.id.logout_from_acct_page) Button logout_button;
+    @Bind(R.id.user_label) TextView user_label;
 
     public AccountSettingsFragment() {
         // Required empty public constructor
@@ -74,5 +70,42 @@ public class AccountSettingsFragment extends android.support.v4.app.Fragment {
         return view;
     }
 
+    @OnClick(R.id.logout_from_acct_page)
+    public void logout() {
+        Firebase ref = ((BracketMasterApplication) getActivity().getApplication()).myFirebaseRef;
+        ref.unauth();
+        Intent intent = new Intent(this.getActivity(), LoginActivity.class);
+        startActivity(intent);
+        getActivity().finish();
+    }
+
+    @OnClick(R.id.change_email_btn)
+    public void changeEmail() {
+        Log.v(TAG, "FUCK");
+        LayoutInflater inflater = getLayoutInflater(null);
+        View dialogView = inflater.inflate(R.layout.dialog_change_email, null);
+        final TextInputLayout emailField = (TextInputLayout)dialogView.findViewById(R.id.change_email_field);
+        final TextInputLayout passwordField = (TextInputLayout)dialogView.findViewById(R.id.change_email_password);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this.getContext())
+                .setTitle("Change Email")
+                .setView(dialogView)
+                .setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if (emailField.getEditText() != null && passwordField.getEditText() != null) {
+                            String newEmail = emailField.getEditText().getText().toString();
+                            String newPassword = passwordField.getEditText().getText().toString();
+                            Log.v(TAG, "New Password = " + newPassword);
+                            Log.v(TAG, "New Email = " + newEmail);
+                            // TODO: Save new email to Firebase (ARYA)
+                            dialogInterface.dismiss();
+                        }
+
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+        dialog.show();
+    }
 
 }
